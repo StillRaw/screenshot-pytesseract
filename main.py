04539ba694630d3ef42fe97f905252b01d242606
StillRaw_Home <onurgulsun@hotmail.com>
2022-12-16 20:02:57 +0300

    Space fixed

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import time
 import os
 import threading
 
+
 class App:
     def __init__(self, root):
         self.root = root
@@ -168,47 +169,50 @@ class App:
 class CopyPaste:
     def on_activate_copy():
         print("<CTRL> + C pressed!")
-        all_text=None
+        all_text = None
         time.sleep(0.1)
         all_text = pyperclip.paste()
         print(all_text)
-        all_text=all_text.replace("\r","")
-        all_text=all_text.replace("\n","")
+        all_text = all_text.replace("\r", "")
+        all_text = all_text.replace("\n", "")
         # is_tc=False
 
         def isValidTCID(value):
-            if value == None or value=="" or value==" ":
+            if value == None or value == "" or value == " ":
                 return False
 
             value = str(value)
-            
+
             # 11 hanelidir.
             if not len(value) == 11:
                 return False
-            
+
             # Sadece rakamlardan olusur.
             if not value.isdigit():
                 return False
-            
+
             # Ilk hanesi 0 olamaz.
             if int(value[0]) == 0:
                 return False
-            
+
             digits = [int(d) for d in str(value)]
-            
+
             # 1. 2. 3. 4. 5. 6. 7. 8. 9. ve 10. hanelerin toplamından elde edilen sonucun
             # 10'a bölümünden kalan, yani Mod10'u bize 11. haneyi verir.
             if not sum(digits[:10]) % 10 == digits[10]:
                 return False
-            
+
             # 1. 3. 5. 7. ve 9. hanelerin toplamının 7 katından, 2. 4. 6. ve 8. hanelerin toplamı çıkartıldığında,
             # elde edilen sonucun 10'a bölümünden kalan, yani Mod10'u bize 10. haneyi verir.
-            if not (((7 * sum(digits[:9][-1::-2])) - sum(digits[:9][-2::-2])) % 10) == digits[9]:
+            if (
+                not (((7 * sum(digits[:9][-1::-2])) - sum(digits[:9][-2::-2])) % 10)
+                == digits[9]
+            ):
                 return False
-            
+
             # Butun kontrollerden gecti.
             return True
-    
+
         if isValidTCID(all_text):
             pyperclip.copy(all_text)
             print(all_text)
